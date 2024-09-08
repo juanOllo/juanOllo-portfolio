2c40677e638af97dfbc1813bc8329b4ce6a6df10
juanOllo <juan_ollo@yahoo.com>
2024-09-08 20:03:52 -0300

    portafolio v2, faltan las fuentes

---
+++ script.js
@@ -0,0 +1,6 @@
+const mailClick = document.getElementById("mail");
+const mymailText = document.getElementById("mymail");
+
+mailClick.addEventListener("click", () => {
+    mymailText.innerText = "juan_ollo@yahoo.com"
+})
